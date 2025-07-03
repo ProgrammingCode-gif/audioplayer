e5f0f5352647cb03edd7c53d842c720b6a2f55b9
Shahzod <shahzodtashev49@gmail.com>
2025-07-03 23:01:52 +0500

    .

--- src/features/player/components/PlayerOptions.tsx
+++ src/features/player/components/PlayerOptions.tsx
@@ -3,15 +3,29 @@ import { FaCirclePlay } from "react-icons/fa6";
 import { FaCirclePause } from "react-icons/fa6";
 import { BiSolidSkipNextCircle } from "react-icons/bi";
 import { BiSolidSkipPreviousCircle } from "react-icons/bi";
+import React from "react";
 
 type Props = {
-    togglePlay: () => void;
     track: Track | null;
     isPlaying: boolean;
+    switchPlay: () => void;
+    audioRef: React.RefObject<HTMLAudioElement | null>;
 }
 
-const PlayerOptions = ({togglePlay, track, isPlaying}: Props) => {
-  return (
+const PlayerOptions = ({track, isPlaying, audioRef, switchPlay }: Props) => {
+
+    const togglePlay = () => {
+        const audio = audioRef.current;
+        if (!audio) return;
+        if(isPlaying) {
+            audio.pause();
+        } else {
+            audio.play();
+        }
+        switchPlay();
+    }
+
+    return (
         <div className="flex jcenter items-center gap-2">
             <button>
                 <BiSolidSkipPreviousCircle className="text-white text-2xl size-8 hover:text-neutral-200 transition-all duration-150 cursor-pointer" />
@@ -22,15 +36,15 @@ const PlayerOptions = ({togglePlay, track, isPlaying}: Props) => {
                 className="p-2 rounded-full transition-colors duration-200 cursor-pointer"
             >
                 {isPlaying ?
-                <FaCirclePause className="text-white text-2xl size-8 transition-all duration-150" /> :
-                <FaCirclePlay className="text-white text-2xl size-8 transition-all duration-150" /> 
+                    <FaCirclePause className="text-white text-2xl size-8 transition-all duration-150" /> :
+                    <FaCirclePlay className="text-white text-2xl size-8 transition-all duration-150" />
                 }
             </button>
             <button>
                 <BiSolidSkipNextCircle className="text-white text-2xl size-8 hover:text-neutral-200 transition-all duration-150 cursor-pointer" />
             </button>
         </div>
-  )
+    )
 }
 
 export default PlayerOptions
--- src/features/player/components/ProgressBar.tsx
+++ src/features/player/components/ProgressBar.tsx
@@ -2,24 +2,102 @@
 
 import { formatTime } from "@/shared/halpers/formatTime"
 import { Track } from "@/shared/types"
-import React, { useRef, useState } from "react";
+import React, { MouseEventHandler, useEffect, useRef, useState } from "react";
 
 type Props = {
     track: Track | null;
     progressTime: number;
     progress: number;
-    setProgressTime?: (progressTime: number) => void | null;
-    setProgress?: (progress: number) => void | null;
+    setProgressTime: (progressTime: number) => void | null;
+    setProgress: (progress: number) => void | null;
     audioRef: React.RefObject<HTMLAudioElement | null>;
 }
 
 const ProgressBar = ({ track, progressTime, progress, setProgress, setProgressTime, audioRef }: Props) => {
+    const barRef = useRef<HTMLDivElement | null>(null)
+    const [tempProgress, setTempProgress] = useState<number>(0)
+    const [dragging, setDragging] = useState(false)
+
+    useEffect(() => {
+        if (!track || !audioRef.current) return;
+
+        const audio = audioRef.current;
+
+        const handleTimeUpdate = () => {
+            console.log(dragging);
+
+            if (!dragging && audio) {
+                const percentage = (audio.currentTime || 0) / (audio.duration || 1) * 100;
+                setProgress(percentage);
+            }
+        };
+        audio.addEventListener('timeupdate', handleTimeUpdate);
+
+        const handleMouseMove = (e: MouseEvent) => {
+            const percent = getPercent(e.clientX);
+            setTempProgress(percent * 100);
+            setProgress(percent * 100);
+            console.log("Mouse move event triggered", progress, percent, dragging, tempProgress);
+        }
+    
+        const handleMouseUp = (e: MouseEvent) => {
+            console.log("Mouse up event triggered", dragging, tempProgress);
+            
+            if (dragging && audioRef.current && track) {
+                const time = getPercent(e.clientX);
+                console.log(time);
+                
+                const newTime = time * track.duration;
+                setProgressTime(newTime);
+                setProgress(tempProgress * 100);
+                audioRef.current.currentTime = newTime;
+            }
+            setDragging(false);
+            setTempProgress(0);
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+        }
+
+        if(dragging) {
+            document.addEventListener('mousemove', handleMouseMove);
+            document.addEventListener('mouseup', handleMouseUp);
+        }
+
+        return () => {
+            audio.removeEventListener('timeupdate', handleTimeUpdate);
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+        };
+    }, [track, dragging]);
+
+    const getPercent = (clientX: number) => {
+        if (!barRef.current) return 0;
+        const rect = barRef.current.getBoundingClientRect()
+        const offsetX = clientX - rect.left
+        const percent = Math.min(Math.max(offsetX / rect.width, 0), 1)
+        return percent
+    }
+
+    const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        const percent = getPercent(e.clientX)
+        if (audioRef.current && track) {
+            audioRef.current.currentTime = percent * track.duration || 0
+        }
+        setProgress(percent * 100)
+    }
+
+    const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (!audioRef.current) return;
+        setDragging(true);
+    }
 
 
     return (
         <div className="flex justify-between items-center gap-4">
-            <span className="text-xs text-[#ffffff82]">{track && formatTime(progressTime)}</span>
+            <span className="text-xs text-[#ffffff82]">{audioRef.current?.currentTime}</span>
             <div
+                ref={barRef}
+                onClick={handleClick}
                 className="relative w-lg h-1 bg-neutral-700 rounded cursor-pointer select-none group"
             >
                 <div
@@ -28,8 +106,8 @@ const ProgressBar = ({ track, progressTime, progress, setProgress, setProgressTi
                 />
 
                 <div
-                    className="absolute top-1/2 -translate-y-1/2 w-3 h-3 bg-white rounded-full shadow-lg 
-             opacity-0 group-hover:opacity-100"
+                    onMouseDown={handleMouseDown}
+                    className={`absolute top-1/2 -translate-y-1/2 w-3 h-3 bg-white rounded-full shadow-lg ${dragging ? 'opacity-100' : 'opacity-0'} group-hover:opacity-100`}
                     style={{ left: `calc(${progress}% - 0.5rem)` }}
                 />
             </div>
